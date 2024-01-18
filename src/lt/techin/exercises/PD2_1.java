3c022c68409bef571bf1ebfab7a1817b2cda67d9
Ticker <rs5@inbox.lt>
2024-01-18 20:32:54 +0200

    PD2_1

--- src/lt/techin/exercises/PD2_1.java
+++ src/lt/techin/exercises/PD2_1.java
@@ -1,9 +1,17 @@
 package lt.techin.exercises;
 
+import java.util.Scanner;
+
 public class PD2_1 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
         System.out.println("Learning Java Now");
         System.out.println("Programming is fun");
+
+        System.out.println("a\t\tb\tc");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please impute your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
     }
 }
---
+++ src/lt/techin/exercises/automobilis.java
@@ -0,0 +1,27 @@
+package lt.techin.exercises;
+
+public class automobilis {
+    String marke;
+    String modelis;
+    int metai;
+
+
+
+    public automobilis(String marke) {
+        this.marke = marke;
+        this.modelis = modelis;
+        this.metai = metai;
+
+
+    }
+    public String getMarke() {
+        return marke;
+
+    }
+
+    public void setMarke(String marke) {
+        this.marke = marke;
+    }
+
+}
+
